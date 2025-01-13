98d8748621dd7732ca15ec1f7e77c605c8212482
KartToonOFF <95185459+KartToonOFF@users.noreply.github.com>
2025-01-13 09:53:17 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from tkinter import *
-from tkinter import filedialog
+from customtkinter import *
+from customtkinter import filedialog
+from PIL import Image
 import shutil
 
 def import_file():
@@ -8,21 +9,24 @@ def import_file():
     if file_path:
         # Donne le chemin du fichiers
         print("Selected file:", file_path)
-        print(shutil.copy(src=file_path, dst="projet/appWindows/files"))
-        canvas.create_image(20,20, anchor=NW, image=f"projet/appWindows/files/")
-
+        copy_path = shutil.copy(src=file_path, dst="projet/appWindows/files")
+        print(copy_path)
+        image = CTkImage(light_image=Image.open(copy_path), dark_image=Image.open(copy_path), size=(100, 100))
+        my_label.configure(image=image)
+        
+copy_path = ""
 file_path = ""
 
-window = Tk()
+window = CTk()
 window.geometry("700x500")
 
-frame = Frame(window, height=500, width=700)
+frame = CTkFrame(window)
 frame.pack()
 
-importButton = Button(frame, text="Import file", command=import_file)
+importButton = CTkButton(frame, text="Import file", command=import_file)
 importButton.pack()
 
-canvas = Canvas(frame, width = 300, height = 300)
-canvas.pack()
+my_label = CTkLabel(frame, text="", image="")
+my_label.pack(pady=10)
 
 window.mainloop()
